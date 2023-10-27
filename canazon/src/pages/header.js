454f0ea03f8f2bf459792a71c1e0176c4c22778b
Cannon7 <roa22002@byui.edu>
2023-10-26 18:04:27 -0600

    gf

---
+++ canazon/src/pages/header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Header(){
+
+    return(
+        <>
+            <img src='https://placehold.co/100x100' alt='shark logo'></img>
+            <h1>Shark </h1>
+            <h3>Fun Shark stuff here</h3>
+            {/* placeholder line thing cuz i need it haha */}
+            <hr></hr>
+        </>
+    );
+}
+
+export default Header;
